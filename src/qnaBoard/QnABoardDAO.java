b7aae08be2fe02fc7cd41cae294f80ad1b41f718
PC-11 <PC-11@DESKTOP-P9R6DQ3>
2021-07-15 21:14:27 +0900

    .

--- src/qnaBoard/QnABoardDAO.java
+++ src/qnaBoard/QnABoardDAO.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import selectMenu.JDBCUtil;
+
 public class QnABoardDAO {
 	private QnABoardDAO() {
 	}
@@ -16,47 +18,39 @@ public class QnABoardDAO {
 		}
 		return instance;
 	}
-
-	public List<QnABoardDTO> selectQnABoard(String memberId){
-		String sql = "SELECT BOARD_NO, MEM_ID, Q_DATE, Q_TITLE"
-								+ " FROM QNA_BOARD "
-								+ "	WHERE MEM_ID = ?";
-
-
-		return instance.selectQnABoard()
-	}
-
-	// public List<Map<String, Object>> selectQnABoard(Map<String, Object> param) {
-//		String sql = "SELECT BOARD_NO, MEM_ID, Q_DATE, Q_TITLE"
-//							 + " FROM QNA_BOARD "
-//							 + "	WHERE MEM_ID = ?";
-//		List<Object> list = new ArrayList<>();
-//		list.add(param.get("MEM_ID"));
-//				
-//		return jdbc.selectList(sql, list);
-//	}
-//	
-//	public int insertQnABoard(Map<String, Object> param) {
-//		String sql = "INSERT INTO "
-//							+ "			QNA_BOARD"
-//							+ " 	 VALUES (SEQ_BOARD_NO.NEXTVAL, ?, SYSDATE, ?, ?)";
-//
-//		List<Object> list = new ArrayList<>();
-//		list.add(param.get("MEM_ID"));
-//		list.add(param.get("Q_TITLE"));
-//		list.add(param.get("Q_CONTENT"));
-//				
-//		return jdbc.update(sql, list);
-//	}
-//	
-//	public int deleteQnABoard(Map<String , Object> param) {
-//		String sql = "DELETE FROM QNA_BOARD"
-//							 + "		WHERE BOARD_NO = ? ";
-//		
-//		List<Object> list = new ArrayList<>();
-//		list.add(param.get("BOARD_NO"));
-//		
-//		return jdbc.update(sql,list);
-//	}
+		private JDBCUtil jdbc = JDBCUtil.getInstance();
+		
+		public boolean insertQnABoard(QnABoardDTO boardInfo) {
+			String sql =  "INSERT INTO "
+									+ "			QNA_BOARD"
+									+ " 	 VALUES (SEQ_BOARD_NO.NEXTVAL, ?, SYSDATE, ?, ?)";
+			
+			List<Object> list = new ArrayList<>();
+			list.add(boardInfo.getMemberId());
+			list.add(boardInfo.getQnaTitle());
+			list.add(boardInfo.getQnaContent());
+			
+			if (jdbc.update(sql, list) == 1) {
+				return true;
+			}
+			return false;
+		}
+		
+		public QnABoardDTO getQnaBoard(String memberId) {
+			QnABoardDTO boardDTO = new QnABoardDTO();
+			String sql = "SELECT * FROM QNA_BOARD"
+								+ "        WHERE MEM_ID= ? ";
+			List<Object> list = new ArrayList<>();
+			list.add(memberId);
+			
+			List<Map<String,Object>> map = jdbc.selectList(sql, list);
+			boardDTO.setMemberId((String)map.get("BOARD_NO"));
+			
+			
+			
+			
+		}
+	
 
 }
+
