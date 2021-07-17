76acff357736303582d4a71c776db32e81c3f1d3
Onyx <Onyx@118.44.158.84>
2021-07-17 15:39:27 +0900

    21-07-17

--- wishList/WishListDTO.java
+++ wishList/WishListDTO.java
@@ -1,10 +1,69 @@
 package wishList;
 
+import java.util.Objects;
+
 public class WishListDTO {
 	
 	private String memberId; //아이디
 	private String productId; //상품코드
-	private int wishListAddDate; //위시리스트 추가일
+	private String wishListAddDate; //위시리스트 추가일
+	
+	public WishListDTO(String memberId, String productId, String wishListAddDate) {
+		super();
+		this.memberId = memberId;
+		this.productId = productId;
+		this.wishListAddDate = wishListAddDate;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public String getWishListAddDate() {
+		return wishListAddDate;
+	}
+
+	public void setWishListAddDate(String wishListAddDate) {
+		this.wishListAddDate = wishListAddDate;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(memberId, productId, wishListAddDate);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WishListDTO other = (WishListDTO) obj;
+		return Objects.equals(memberId, other.memberId) && Objects.equals(productId, other.productId)
+				&& wishListAddDate == other.wishListAddDate;
+	}
+
+	@Override
+	public String toString() {
+		return "WishListDTO [memberId=" + memberId + ", productId=" + productId + ", wishListAddDate=" + wishListAddDate
+				+ "]";
+	}
+	
+	
 	
 	
 
---
+++ wishList/WishListService.java
@@ -0,0 +1,54 @@
+package wishList;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import controller.Controller;
+import selectMenu.JDBCUtil;
+import selectMenu.ScanUtil;
+
+public class WishListService {
+
+	private static WishListService instance;
+
+	private WishListService() {
+	}
+
+	public static WishListService getInstance() {
+		if (instance == null) {
+			instance = new WishListService();
+		}
+		return instance;
+	}
+
+	private static JDBCUtil jdbcUtil = JDBCUtil.getInstance();
+	private WishListDAO wishListDAO = WishListDAO.getInstance();
+	
+	
+	public int addWishList() {
+		System.out.println("=========== 위시리스트 등록 =============");
+		//로그인한 멤버의 아이디로 자동 등록
+		String memberId = (String) Controller.loginUser.get("MEM_ID");
+		System.out.print("상품코드를 입력하세요");
+		String productId = ScanUtil.nextLine();
+		//위시리스트 추가일로 자동 날짜 등록
+		String wishListAddDate = "SYSDATE";
+		
+
+		Map<String, Object> param = new HashMap<>();
+		param.put("MEM_ID", memberId);
+		param.put("PROD_ID", productId);
+		param.put("WISH_DATE", wishListAddDate);
+
+		int result = WishListDAO.insertWishList(param);
+
+		if (0 < result) {
+			System.out.println("위시리스트 등록에 성공했습니다.");
+		} else {
+			System.out.println("위시리스트 등록에 실패했습니다.");
+		}
+
+		return addWishList();
+	}
+
+}
