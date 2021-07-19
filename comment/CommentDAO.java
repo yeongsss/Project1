ecfd69cf1089f5884e5703ebd91a3e97e5dd3d2c
onyx <bhm1200@naver.com>
2021-07-19 12:51:57 +0900

    # WARNING: head commit changed in the meantime
    21-07-19

---
+++ comment/CommentDAO.java
@@ -0,0 +1,83 @@
+package comment;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+import selectMenu.JDBCUtil;
+
+public class CommentDAO {
+	private CommentDAO() {}
+	
+	private static CommentDAO instance;
+	public static CommentDAO getInstance() {
+		if (instance ==null) {
+			instance = new CommentDAO();
+		}
+		return instance;
+	}
+	
+	private JDBCUtil jdbc =  JDBCUtil.getInstance();
+	
+	public boolean insertComment(CommentDTO comment ) {
+		String sql = "INSERT INTO CMNTS"
+							+ "    VALUES (? ,?,SYSDATE,?)";
+		
+		List<Object> list = new ArrayList<>();
+		list.add(comment.getBoardNo());
+		list.add(comment.getMemberId());
+		list.add(comment.getCommentContent());
+		
+		
+		if (jdbc.update(sql, list)==1) {
+			return true;
+		}
+		return false;
+	}
+	
+	public List<Map<String, Object>> getComment(int boardNo) {
+		String sql = "SELECT * FROM CMNTS"
+							+ "        WHERE BOARD_NO= ? ";
+		List<Object> param = new ArrayList<>();
+		param.add(boardNo);
+		return jdbc.selectList(sql, param);
+		
+		 
+	}	
+	
+	public List<Map<String , Object>> allComment() {
+		CommentDTO commentDTO = new CommentDTO();
+		String sql = "SELECT * FROM CMNTS";
+		List<Map<String , Object>> list = jdbc.selectList(sql);
+		return list;
+	}
+	
+
+	public Map<String, Object> deleteComment (int boardNo) {
+		String sql = "DELETE FROM CMNTS"
+								+ "WHERE BOARD_NO= ?";
+		List<Object> list = new ArrayList<>();
+		list.add(boardNo);
+		return jdbc.selectOne(sql) ;
+	}
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ comment/CommentDTO.java
@@ -0,0 +1,104 @@
+package comment;
+
+public class CommentDTO {
+	private int boardNo;
+	private String memberId;
+	private int commentDate;
+	private String commentContent;
+	
+	public CommentDTO() {
+	}
+
+	public CommentDTO(int boardNo, String memberId, String commentContent) {
+		super();
+		this.boardNo = boardNo;
+		this.memberId = memberId;
+		this.commentDate = commentDate;
+		this.commentContent = commentContent;
+	}
+
+	
+	public CommentDTO(int boardNo, String memberId, int commentDate, String commentContent) {
+		super();
+		this.boardNo = boardNo;
+		this.memberId = memberId;
+		this.commentDate = commentDate;
+		this.commentContent = commentContent;
+	}
+
+	public int getBoardNo() {
+		return boardNo;
+	}
+
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public int getCommentDate() {
+		return commentDate;
+	}
+
+	public void setCommentDate(int commentDate) {
+		this.commentDate = commentDate;
+	}
+
+	public String getCommentContent() {
+		return commentContent;
+	}
+
+	public void setCommentContent(String commentContent) {
+		this.commentContent = commentContent;
+	}
+
+	@Override
+	public String toString() {
+		return "commentDTO [boardNo=" + boardNo + ", memberId=" + memberId + ", commentDate=" + commentDate
+				+ ", commentContent=" + commentContent + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + boardNo;
+		result = prime * result + ((commentContent == null) ? 0 : commentContent.hashCode());
+		result = prime * result + commentDate;
+		result = prime * result + ((memberId == null) ? 0 : memberId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CommentDTO other = (CommentDTO) obj;
+		if (boardNo != other.boardNo)
+			return false;
+		if (commentContent == null) {
+			if (other.commentContent != null)
+				return false;
+		} else if (!commentContent.equals(other.commentContent))
+			return false;
+		if (commentDate != other.commentDate)
+			return false;
+		if (memberId == null) {
+			if (other.memberId != null)
+				return false;
+		} else if (!memberId.equals(other.memberId))
+			return false;
+		return true;
+	}
+	
+}
